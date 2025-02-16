91cab40c9da22836ee2174974cde552ea308c194
Lucas Matricarde <lucas.eduardo@wises.com.br>
2025-02-16 11:12:41 -0300

    Arrumando margin

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,13 +1,13 @@
 export const dataFake = [
   {
     'id' : "1",
-    'title' : 'Meus Carros',
+    'title' : 'Conheca o Angular',
     'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec arcu sed ipsum luctus egestas. Duis pharetra erat et facilisis fermentum. Fusce tempor nec sem nec aliquet. Morbi rhoncus massa id urna bibendum scelerisque.',
     'pictureCover' : 'https://external-preview.redd.it/qW5sXZSe_7w815bbdJh3mwhzBxyEdK13fFyRs3_8ZdQ.jpg?width=640&crop=smart&auto=webp&s=53d250c9db70f2b0fe7a93de0cf57e7f7ffc2a06'
   },
   {
     'id' : "2",
-    'title' : 'Minhas Motos',
+    'title' : 'Novo no Angular',
     'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec arcu sed ipsum luctus egestas. Duis pharetra erat et facilisis fermentum. Fusce tempor nec sem nec aliquet. Morbi rhoncus massa id urna bibendum scelerisque.',
     'pictureCover' : 'https://ionic.io/blog/wp-content/uploads/2024/02/angular-feature-image-1-1024x512.png'
   }
